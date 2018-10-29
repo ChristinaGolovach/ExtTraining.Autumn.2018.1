b9a2ab0ef7e3eb112f7f24bce5c8b82068099cdc
Christina <christina.golovach@gmail.com>
2018-10-29 10:19:45 +0300

    Add BookExtension

--- BookExtension/BookFormatExtension.cs
+++ BookExtension/BookFormatExtension.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using BookLibrary;
 
 namespace BookExtension
 {
-    public class BookFormatExtension
+    public class BookFormatExtension : IFormatProvider, ICustomFormatter
     {
+        IFormatProvider parentformatProvider;
+
+        public BookFormatExtension() : this(CultureInfo.InvariantCulture) { }
+
+        public BookFormatExtension(IFormatProvider parentFormatProvoder)
+        {
+            parentformatProvider = parentFormatProvoder;   
+        }
+
+        public string Format(string format, object argument, IFormatProvider formatProvider)
+        {
+            string inputFormat = format.ToUpperInvariant();
+            if (argument == null || inputFormat != "NEW" || argument.GetType() != typeof(Book))
+            {
+                return string.Format(parentformatProvider, "{0:" + format + "}", argument);
+            }
+
+            Book book = (Book)argument;
+
+            string resultNewFormat = "Author is " + book.Author.ToString();
+
+            return resultNewFormat;
+             
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+
+            else
+            {
+                return null;
+            }
+        }
     }
 }
